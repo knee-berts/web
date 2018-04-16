7822eec1bc003d4aed825f4e9e6b5ad745fe46ca
Nicholas Eberts <nicholas.eberts@yahoo.com>
2018-04-16 17:56:12 -0400

    cicd test

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Kubernetes and MS sitting in trees
+        Kubernetes is all up on Azure
       </div>
       <div class="col-lg-6">
       </div>
